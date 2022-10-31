ac12c14e972c8dfac7a807285b1a850360d95eb2
unknown <cristiana.farias@etec.sp.gov.br>
2022-10-31 20:26:12 -0300

    Projeto2

---
+++ crfapp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crfapp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string sobrenome;
+            Console.WriteLine("digite seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("digite seu sobrenome");
+            sobrenome = Console.ReadLine();
+            Console.WriteLine(nome +" "+sobrenome);
+            Console.ReadKey();
+        }
+    }
+}
